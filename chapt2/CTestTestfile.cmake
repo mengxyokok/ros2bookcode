9ad972ffb6e033de67f60719852d3b1d19c013d7
mxy <mengxyokok@gmail.com>
2025-08-04 15:17:28 +0800

    chapt2 编译通过

---
+++ chapt2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mxy/robot/fishbot/ros2bookcode/chapt2
+# Build directory: /home/mxy/robot/fishbot/ros2bookcode/chapt2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
